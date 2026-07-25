3b2b541c92b618262c588abb60b23a7aa61bf9e6
moqbel <yabd390@gmail.com>
2026-07-26 00:25:37 +0300

    project creation

---
+++ src/main/java/model/employee.java
@@ -0,0 +1,34 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "employee")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID employeeId;
+
+    @Column (nullable = false)
+    private String firstName;
+
+    @Column (nullable = false)
+    private String lastName;
+
+    @Column (nullable = false, unique = true)
+    private String email;
+
+    @Column (nullable = false)
+    private String password;
+
+}
